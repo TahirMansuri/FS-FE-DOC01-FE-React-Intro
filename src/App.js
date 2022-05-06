7d48f803b69eeca5d04aaa5509a0fa853d8f7761
TahirMansuri <tprocks786@gmail.com>
2022-05-06 14:00:37 +0530

    ADDED: Displaying Greeting Message using One Way Binding-state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
+
+  constructor(){
+    super()
+    this.state = {
+      title : "Hello World from Tahir Mansuri."
+    }
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello World from Tahir Mansuri.</h1>
+        <h1>{this.state.title}</h1>
       </div>
     );
   }
